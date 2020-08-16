6fe42fea83e0bc776c033c20a78a6f698b8bff41
miguel <miguelmu@kth.se>
2020-08-17 01:14:23 +0200

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cthulu-client "0.1.0-SNAPSHOT"
+(defproject cthulu-client "0.1.0"
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/clojurescript "1.10.773"
                   :exclusions [com.google.javascript/closure-compiler-unshaded
